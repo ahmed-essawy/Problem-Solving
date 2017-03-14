291d21ce96bcf20fc5a97ff2d6ab0178a293af94
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-03-14 20:15:43 +0200

    Contest Hall Preparation

---
+++ A2 Online Judge/2 - Little Experience/Contest Hall Preparation/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Contest_Hall_Preparation
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            int[][,] nums = new int[T][,];
+            int[] counters = new int[T];
+            for (int i = 0; i < T; i++)
+            {
+                int[] dim = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                nums[i] = new int[dim[0], dim[1]];
+                for (int x = 0; x < nums[i].GetLength(0); x++)
+                {
+                    int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                    for (int y = 0; y < nums[i].GetLength(1); y++)
+                    {
+                        nums[i][x, y] = arr[y];
+                    }
+                }
+            }
+            Console.Clear();
+            for (int i = 0; i < T; i++)
+            {
+                HashSet<int> list = new HashSet<int>();
+                for (int x = 0, x_len = nums[i].GetLength(0); x < x_len; x++)
+                {
+                    for (int y = 0, y_len = nums[i].GetLength(1); y < y_len; y++)
+                    {
+                        if (nums[i][x, y] != -1)
+                        {
+                            if (x > 0)
+                            {
+                                if (y > 0)
+                                    if (nums[i][x, y] == nums[i][x - 1, y - 1]) { list.Add(nums[i][x, y]); }
+                                if (nums[i][x, y] == nums[i][x - 1, y]) { list.Add(nums[i][x, y]); }
+                                if (y < y_len - 1)
+                                    if (nums[i][x, y] == nums[i][x - 1, y + 1]) { list.Add(nums[i][x, y]); }
+                            }
+
+                            if (y > 0)
+                                if (nums[i][x, y] == nums[i][x, y - 1]) { list.Add(nums[i][x, y]); }
+                            if (y < y_len - 1)
+                                if (nums[i][x, y] == nums[i][x, y + 1]) { list.Add(nums[i][x, y]); }
+
+                            if (x < x_len - 1)
+                            {
+                                if (y > 0)
+                                    if (nums[i][x, y] == nums[i][x + 1, y - 1]) { list.Add(nums[i][x, y]); }
+                                if (nums[i][x, y] == nums[i][x + 1, y]) { list.Add(nums[i][x, y]); }
+                                if (y < y_len - 1)
+                                    if (nums[i][x, y] == nums[i][x + 1, y + 1]) { list.Add(nums[i][x, y]); }
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(list.Count());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ A2 Online Judge/2 - Little Experience/Contest Hall Preparation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("22. Contest Hall Preparation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("22. Contest Hall Preparation")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9df6126c-5014-4f4d-81f2-55377c6a9f8f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
