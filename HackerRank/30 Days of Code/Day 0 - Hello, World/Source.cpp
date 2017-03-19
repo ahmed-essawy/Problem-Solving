103c593035aa1dbc345c56cb2f4c8357662537be
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-03-19 22:33:52 +0200

    Day 0: Hello, World.

---
+++ HackerRank/30 Days of Code/Day 0 - Hello, World/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string input_string;
+	getline(cin, input_string);
+	cout << "Hello, World." << endl << input_string;
+	return 0;
+}
