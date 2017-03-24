10e37c5e6a70c97d2f4401836aff7c578b351fd3
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-03-24 19:29:46 +0200

    Strings

---
+++ HackerRank/C++/Strings/Strings/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+	cout << to_string(a.length()) + " " + to_string(b.length()) << endl;
+	cout << a + b << endl;
+	cout << b[0];
+	for (int i = 1; i < a.length(); i++)
+		cout << a[i];
+	cout << " " << a[0];
+	for (int i = 1; i < b.length(); i++)
+		cout << b[i];
+	return 0;
+}
