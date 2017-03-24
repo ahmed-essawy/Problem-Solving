b30c73e014c9c9585c459dc030ca406351bd642c
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-03-24 20:04:59 +0200

    StringStream

---
+++ HackerRank/C++/Strings/StringStream/Source.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	vector<int> integers;
+	stringstream ss(str);
+	int i;
+	while (ss >> i)
+	{
+		integers.push_back(i);
+		if (ss.peek() == ',')
+			ss.ignore();
+	}
+	return integers;
+}
+
+int main() {
+	string str;
+	cin >> str;
+	vector<int> integers = parseInts(str);
+	for (int i = 0; i < integers.size(); i++) {
+		cout << integers[i] << "\n";
+	}
+	return 0;
+}
